2f9207e66aa5b8012d6b37c150cdb4e4764fccf8
Amir Naderi <amirnaderie@gmail.com>
2024-11-20 17:23:04 +0330

    cahnge nextConfig

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/**
+ *  @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  /* config options here */
+  images: {
+    domains: ["185.19.201.148"],
+  },
+};
+
+export default nextConfig;
